7dc326b8407a717300a5c3e76e5f9e9846b2c7c7
Leonardo Galli <leonardo.galli@bluewin.ch>
2021-09-14 17:37:20 +0200

    fix syntax

--- dev_cmd.ps1
+++ dev_cmd.ps1
@@ -2,7 +2,7 @@ function MsDevShell {
     # Use `vswhere` to locate Visual Studio editions.
     $products = 'Community','Professional','Enterprise','BuildTools' | %{ "Microsoft.VisualStudio.Product.$_" }
     $vswhere = Get-Command 'vswhere'
-    $vs = & $vswhere.Path -products $products -latest -version [16.11,16.12) -format json | ConvertFrom-Json
+    $vs = & $vswhere.Path -products $products -latest -version '[16.11,16.12)' -format json | ConvertFrom-Json
     $tools = Join-Path $vs.installationPath 'Common7' 'Tools'
     try {
     # Attempt 1 (Visual Studio 2019 and newer)
@@ -22,27 +22,27 @@ function MsDevShell {
     Import-Module $devshell
     Enter-VsDevShell -VsInstanceId $vs.instanceId -SkipAutomaticLocation -DevCmdArguments '-arch=${{ inputs.architecture }} -no_logo'
     } catch {
-    # Print exception for debugging.
-    echo $_
-    # Attempt 2 (Visual Studio 2017)
-    #
-    # Execute VsDevCmd.bat and parse its output into a collection. Then
-    # take each entry and merge it into the environment. The idea was
-    # taken from:
-    #     https://github.com/microsoft/vswhere/issues/150#issuecomment-485381959
-    # Locate VsDevCmd.bat within the Visual Studio installation.
-    $devcmd = Join-Path $tools 'VsDevCmd.bat'
-    if (!(Test-Path $devcmd -Type Leaf)) {
-        throw "error: cannot find VsDevCmd.bat"
-    }
-    # Run VsDevCmd.bat and parse the output into a collection.
-    $cmd = '"{0}" -arch=${{ inputs.architecture }} -no_logo && pwsh -Command "Get-ChildItem env: | Select-Object Name,Value | ConvertTo-Json"' -f $devcmd
-    $output = $(& "${env:COMSPEC}" /s /c $cmd)
-    if ($LASTEXITCODE -ne 0) {
-        throw $output
-    }
-    # Merge the output into the environment.
-    $output | ConvertFrom-Json | %{ Set-Content "env:$($_.Name)" $_.Value }
+        # Print exception for debugging.
+        echo $_
+        # Attempt 2 (Visual Studio 2017)
+        #
+        # Execute VsDevCmd.bat and parse its output into a collection. Then
+        # take each entry and merge it into the environment. The idea was
+        # taken from:
+        #     https://github.com/microsoft/vswhere/issues/150#issuecomment-485381959
+        # Locate VsDevCmd.bat within the Visual Studio installation.
+        $devcmd = Join-Path $tools 'VsDevCmd.bat'
+        if (!(Test-Path $devcmd -Type Leaf)) {
+            throw "error: cannot find VsDevCmd.bat"
+        }
+        # Run VsDevCmd.bat and parse the output into a collection.
+        $cmd = '"{0}" -arch=${{ inputs.architecture }} -no_logo && pwsh -Command "Get-ChildItem env: | Select-Object Name,Value | ConvertTo-Json"' -f $devcmd
+        $output = $(& "${env:COMSPEC}" /s /c $cmd)
+        if ($LASTEXITCODE -ne 0) {
+            throw $output
+        }
+        # Merge the output into the environment.
+        $output | ConvertFrom-Json | %{ Set-Content "env:$($_.Name)" $_.Value }
     }
 }
 # Enter VsDevShell and collect the environment before and after.
