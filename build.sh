b8d2a984819abd4df7d7555a622c6e4f1aaa4f4a
Leonardo Galli <leonardo.galli@bluewin.ch>
2021-09-15 20:16:53 +0200

    fix

--- build.sh
+++ build.sh
@@ -4,12 +4,13 @@ echo "[*] Building nmips plugin at $BUILD_DIR!"
 rm -rf $BUILD_DIR || echo "No existing build dir, sadge"
 mkdir $BUILD_DIR
 
-SDK_DOWNLOAD="https://hex-rays.com/products/ida/support/ida/idasdk76.zip"
-# echo "[*] Decrypting idasdk..."
-# echo $IDA_PASS | age -d -o $BUILD_DIR/idasdk.zip idasdk76.zip
-# openssl aes-256-cbc -d -md sha256 -in idasdk.zip -out $BUILD_DIR/idasdk.zip -pass env:IDA_PASS
+# SDK_DOWNLOAD="https://hex-rays.com/products/ida/support/ida/idasdk76.zip"
+SDK_DOWNLOAD="https://cdn.vis.ethz.ch/ctf/idasdk.zip"
 echo "[*] Downloading idasdk..."
-wget --http-user=$IDA_USER --http-password=$IDA_PASS $SDK_DOWNLOAD -O $BUILD_DIR/idasdk.zip
+# wget --http-user=$IDA_USER --http-password=$IDA_PASS $SDK_DOWNLOAD -O $BUILD_DIR/idasdk.zip
+wget $SDK_DOWNLOAD -O $BUILD_DIR/idasdk.enc.zip
+echo "[*] Decrypting idasdk..."
+openssl aes-256-cbc -d -md sha256 -in $BUILD_DIR/idasdk.enc.zip -out $BUILD_DIR/idasdk.zip -pass env:IDA_PASS
 echo "[*] Setting up idasdk..."
 cd $BUILD_DIR
 unzip idasdk
